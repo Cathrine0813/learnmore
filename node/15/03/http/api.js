59e085ccb0e39e3599c091791721267aaa3bbeab
Cathrine0813 <877279334@qq.com>
2020-01-09 18:32:58 +0800

    ce

--- node/15/03/http/api.js
+++ node/15/03/http/api.js
@@ -9,6 +9,8 @@ http.createServer((req, res) => {
     console.log('url:' + url)
     console.log('method:' + method)
 
+    console.log('cookie:' + req.headers.cookie)
+
     if (method == "GET" && url == "/") {
         // 返回一个页面
         fs.readFile("./index.html", (err, data) => {
@@ -17,22 +19,50 @@ http.createServer((req, res) => {
         });
 
     } else if (method == "GET" && url == "/api/users") {
-        // 允许访问的源
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+        // 设置cookie，鉴权，设置cookie到浏览器内部，当发生同域请求时会将cookie带到request里面
+        res.setHeader('Set-Cookie','cookie1=ceshihhhh')
+        // res.setHeader('Access-Control-Allow-Credentials','true') //确认接受cookie
 
-        // 返回一个接口 
-        res.setHeader("Content-Type", "application/json");
+        // // 允许访问的源
+        // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+
+        // // 返回一个接口 
+        // res.setHeader("Content-Type", "application/json");
+
         res.end(JSON.stringify([{
             name: "tom",
             age: 20
         }]));
     } else if (method == "OPTIONS" && url == "/api/users") {
-        res.writeHead(200, {
-            "Access-Control-Allow-Origin": "http://localhost:3000",
-            "Access-Control-Allow-Headers": "X-Token,Content-Type",
-            "Access-Control-Allow-Methods": "PUT"
-        });
-        res.end();
+        // res.setHeader('Access-Control-Allow-Credentials','true')
+        // res.writeHead(200, {
+        //     "Access-Control-Allow-Origin": "http://localhost:3000", //允许localhost:3000
+        //     "Access-Control-Allow-Headers": "X-Token,Content-Type", //允许X-Token
+        //     "Access-Control-Allow-Methods": "PUT"
+        // });
+        // res.end();
+    } else if (method === "POST" && url == "/api/save") {
+        // post请求接收body信息，body在request中，而且是个流
+        let reqData = [] // 流的数组
+        let size = 0
+
+        // 接收的body
+        req.on('data', data => {
+            // data是一个二进制的buffer
+            console.log('>>req on', data) 
+            reqData.push(data)
+            size += data.length //记住多大 
+        })
+
+        // 接收完
+        req.on('end', function () { 
+            console.log('end')
+            const data = Buffer.concat(reqData, size) //把所有buffer连接在一起，并且传入size
+            console.log('data', size, data.toString()) //data.toString()实际上就是一个query string字符串
+
+            // 可以解析完返回页面
+            res.end(`formdata:${data.toString()}`)
+        })
     }
 }).listen(4000, () => {
     console.log('api listen at 4000')
--- node/15/03/http/proxy.js
+++ node/15/03/http/proxy.js
@@ -7,6 +7,14 @@ const app = express()
 // express自带静态服务器 地址指向文件index.html根目录
 app.use(express.static(__dirname + '/'))
 
+// npm install --save-dev http-proxy-middleware
+// 建立一个代理（就是反向代理）：加载一个http - proxy - middleware中间件, 用于后台将请求转发给其它服务器。
+const proxy = require('http-proxy-middleware')
+app.use('/api', proxy({
+    target:'http://localhost:4000' //转向到
+}))
+// vue中的vue.config.js中的proxy就是建立了反向代理，这里的devServer其实就是express服务器，这里注册的proxy就是http-proxy-middleware
+
 app.listen(3000, () => {
     console.log('proxy is 3000')
 })
