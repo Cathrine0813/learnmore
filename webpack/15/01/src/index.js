81205670bf1c62351c1556988c9fdebfc46d911c
Cathrine0813 <877279334@qq.com>
2020-01-15 18:36:28 +0800

    jtain

---
+++ webpack/15/01/src/index.js
@@ -0,0 +1,15 @@
+//！ webpack 4.x默认配置，也就是0配置（这个配置是特别弱的）
+//！ 默认只支持js、json格式 
+
+// const add = require('./test')
+
+// // webpack进入入口文件，会分析出依赖、文件、内容， 分析完会生成一个webpack启动函数
+// const a = require('./a') //引入了其他模块，这种是依赖
+
+console.log('hello webpack')
+
+
+// let img = new Image()
+// img.src = './logo.png'
+// let root = document.getElementById('root')
+// root.append(img)
