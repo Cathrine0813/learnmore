aac2bec866c5d541d7687520a8f02be63a840bbb
Cathrine0813 <877279334@qq.com>
2020-01-17 18:31:56 +0800

    02

--- webpack/15/02/src/index.js
+++ webpack/15/02/src/index.js
@@ -1,3 +1,33 @@
+
+import axios from 'axios'
 import css from './index.css'
+import counter from "./counter";
+import number from "./number";
+counter();
+number();
+// js的hmr需要加上监听,
+if (module.hot) {
+    module.hot.accept("./number", function() {
+        document.body.removeChild(document.getElementById("number"));
+        number();
+    });
+}
+
+
+// mock，会有跨域问题
+axios.get('/api/info').then(res => {
+    console.log(res)
+})
+
 
 console.log('hello webpack')
+
+
+var btn = document.createElement("button");
+btn.innerHTML = "新增";
+document.body.appendChild(btn);
+btn.onclick = function() {
+    var div = document.createElement("div");
+    div.innerHTML = "item";
+    document.body.appendChild(div);
+};
--- webpack/15/02/webpack.config.js
+++ webpack/15/02/webpack.config.js
@@ -6,6 +6,9 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 // 抽离css为独立文件
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
+
+const webpack = require("webpack");
+
 module.exports = {
     mode: "development",
     entry: './src/index.js',
@@ -18,8 +21,8 @@ module.exports = {
             // css
             {
                 test: /\.css$/,
-                // use:['style-loader', 'css-loader'] //用了MiniCssExtractPlugin就不能使用style-loader
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
+                use:['style-loader', 'css-loader'] //用了MiniCssExtractPlugin就不能使用style-loader
+                // use: [MiniCssExtractPlugin.loader, 'css-loader']
                 //MiniCssExtractPlugin.loader是MiniCssExtractPlugin自带的loader，是将css以文件的形式输出的
             },
             // 图片
@@ -47,6 +50,22 @@ module.exports = {
     },
     // 字符串类型，不支持函数使用
     devtool:'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: "./dist",// 指定本地服务器的静态资源目录
+        open: true, // 启动本地服务器后是否自动打开浏览器窗口
+        port: 8083,  // 本地服务器端口
+        proxy: {    //代理
+            // 当访问的请求带有key这个字段的时候，就把当前请求转发（代理）到我们用express搭建的服务器那里
+            '/api': {
+                target: "http://localhost:9092",
+                // pathRewrite: { //重写路径
+                //     '^/api': '/'
+                // }
+            }
+        },
+        hot: true,
+        hotOnly:true, //即便HMR不⽣效，浏览器也不⾃动刷新，就开启hotOnly
+    },
     // 引入对应的插件，之后再plugins中进行实例化
     plugins: [
         new HtmlWebpackPlugin({
@@ -57,10 +76,12 @@ module.exports = {
         // 在构建之前将dist目录删除掉
         new CleanWebpackPlugin(),
         // 提取css为独立文件
-        new MiniCssExtractPlugin({
-            // filename: "[name][chunkhash:8].css"
-            // filename: "css/[name].css"
-            filename: "css/[name]_[contenthash:6].css"
-        })
+        // new MiniCssExtractPlugin({
+        //     // filename: "[name][chunkhash:8].css"
+        //     // filename: "css/[name].css"
+        //     filename: "css/[name]_[contenthash:6].css"
+        // }),
+        // webpack自带的热模块更新插件,不⽀持抽离出的css 我们要使⽤css-loader
+        new webpack.HotModuleReplacementPlugin()
     ]
 }
